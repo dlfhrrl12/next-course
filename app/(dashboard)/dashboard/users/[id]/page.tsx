c8ee9112cdd6144d44912028e907dac94f9e158d
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-03-12 16:31:14 +0900

    초기화

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const UserDetailPage = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-  return <div>UserPropile: {id}</div>;
-};
-
-export default UserDetailPage;
